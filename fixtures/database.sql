ba0418a6ae42fe4abf2b743d5f44ea39d2c7bd00
Oscar <oxkhar@gmail.com>
2014-03-14 05:41:14 +0100

    Creando ficheros SQL de creación y carga de datos

--- fixtures/database.sql
+++ fixtures/database.sql
@@ -1,8 +1,15 @@
+/*!40101 SET NAMES utf8 */;
 
+--
+-- Table structure for table `countries`
+--
+
+DROP TABLE IF EXISTS `countries`;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `countries` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `iso`  varchar(2) NOT NULL,
+  `id` int(10) NOT NULL,
+  `iso` char(2) NOT NULL,
+  `name` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 );
 
